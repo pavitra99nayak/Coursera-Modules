e94d10bfa59d7da28f5ba4d12a7b9b7e20f9a70a
Pavitra Nayak <pavitranayak832@gmail.com>
2020-04-27 19:42:27 +0530

    added web pages

--- Module4_assignment/script.js
+++ Module4_assignment/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Pavitra", "John", "Jen", "Jason", "Namitha", "Niveditha", "Laura", "Pavan", "Raquel"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
